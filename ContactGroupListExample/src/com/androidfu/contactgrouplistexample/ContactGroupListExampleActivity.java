756abf9d930bdcad69757eef6a68b2b49bc1947c
bill <bmote@medplus.com>
2012-05-17 12:19:43 -0400

    New functionality for Brandon

--- ContactGroupListExample/src/com/androidfu/contactgrouplistexample/ContactGroupListExampleActivity.java
+++ ContactGroupListExample/src/com/androidfu/contactgrouplistexample/ContactGroupListExampleActivity.java
@@ -16,115 +16,122 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 public class ContactGroupListExampleActivity extends ListActivity {
-	private static final String TAG = "ContactGroupListExampleActivity";
-	List<ContactGroup> groupList;
+    private static final String TAG = "ContactGroupListExampleActivity";
+    private List<ContactGroup> _groupList;
+    private ContactGroupAdapter _adapter;
 
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.main);
-		Log.d(TAG, "onCreate()");
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        Log.d(TAG, "onCreate()");
 
-		this.groupList = getContactGroupList();
-		setListAdapter(new ContactGroupAdapter(this));
-	}
+        this._groupList = getContactGroupList();
+        _adapter = new ContactGroupAdapter(this);
+        setListAdapter(_adapter);
+    }
 
-	public ArrayList<ContactGroup> getContactGroupList() {
-		Log.d(TAG, "getContactGroupList()");
+    public ArrayList<ContactGroup> getContactGroupList() {
+        Log.d(TAG, "getContactGroupList()");
 
-		ArrayList<ContactGroup> groupList = new ArrayList<ContactGroup>();
-		ContactGroup group = new ContactGroup();
-		ContactGroupMember member = new ContactGroupMember();
+        ArrayList<ContactGroup> groupList = new ArrayList<ContactGroup>();
+        ContactGroup group = new ContactGroup();
+        ContactGroupMember member = new ContactGroupMember();
 
-		Log.d(TAG, "Creating a member.");
-		member.name = "Test Member";
-		member.mobilePhoneNumber = "(123) 456-7890";
-		member.id = 1;
+        Log.d(TAG, "Creating a member.");
+        member.name = "Test Member";
+        member.mobilePhoneNumber = "(123) 456-7890";
+        member.id = 1;
 
-		Log.d(TAG, "Creating a group.");
-		group.name = "One Group";
-		group.id = 1;
-		groupList.add(group);
+        Log.d(TAG, "Creating a group.");
+        group.name = "One Group";
+        group.id = 1;
+        groupList.add(group);
 
-		Log.d(TAG, "Returning our list.");
-		return groupList;
-	}
+        Log.d(TAG, "Returning our list.");
+        return groupList;
+    }
 
-	@Override
-	protected void onListItemClick(ListView l, View v, int position, long id) {
-		Log.d(TAG, "onListItemClick()");
-		ContactGroup selectedGroup = new ContactGroup();
-		selectedGroup = groupList.get(position);
-		Toast.makeText(getApplicationContext(),	String.valueOf(selectedGroup.id), Toast.LENGTH_SHORT).show();
-	}
+    @Override
+    protected void onListItemClick(ListView l, View v, int position, long id) {
+        Log.d(TAG, "onListItemClick()");
+        ContactGroup selectedGroup = new ContactGroup();
+        selectedGroup = _groupList.get(position);
+        Toast.makeText(getApplicationContext(), String.valueOf(selectedGroup.id), Toast.LENGTH_SHORT).show();
+        // groupList.removeAll(groupList);
+        _groupList.clear();
+        _adapter.notifyDataSetChanged();
 
-	public class ContactGroupAdapter extends BaseAdapter {
+    }
 
-		public ContactGroupAdapter(Context c) {
-			mContext = c;
-		}
+    public class ContactGroupAdapter extends BaseAdapter {
 
-		@Override
-		public int getCount() {
-			return groupList.size();
-		}
+        public ContactGroupAdapter(Context c) {
+            mContext = c;
+        }
 
-		@Override
-		public Object getItem(int position) {
-			return position;
-		}
+        @Override
+        public int getCount() {
+            return _groupList.size();
+        }
 
-		@Override
-		public long getItemId(int position) {
-			return position;
-		}
+        @Override
+        public Object getItem(int position) {
+            return position;
+        }
 
-		@Override
-		public View getView(int position, View convertView, ViewGroup parent) {
-			ViewHolder holder;
-			
-			if (convertView == null) {
-				LayoutInflater vi = LayoutInflater.from(this.mContext);
-				convertView = vi.inflate(R.layout.listitem, null);
-				holder = new ViewHolder();
-				convertView.setTag(holder);
-			} else {
-				// Get ViewHolder back
-				holder = (ViewHolder) convertView.getTag();
-			}
-			ContactGroup contactGroup = groupList.get(position);
-			if (contactGroup != null) {
-				holder.name = (TextView) convertView.findViewById(R.id.lv_topText);
-				holder.name.setText(contactGroup.name);
-			}
-			return convertView;
-		}
-		private Context mContext;
-	}
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
 
-	public class ViewHolder {
-		TextView name;
-	}
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            ViewHolder holder;
 
-	public class ContactGroup {
-		private static final String TAG = "ContactGroupsClass";
-		public String name;
-		public long id;
-		public List<ContactGroupMember> memberIdList;
+            if (convertView == null) {
+                LayoutInflater vi = LayoutInflater.from(this.mContext);
+                convertView = vi.inflate(R.layout.listitem, null);
+                holder = new ViewHolder();
+                convertView.setTag(holder);
+            } else {
+                // Get ViewHolder back
+                holder = (ViewHolder) convertView.getTag();
+            }
+            ContactGroup contactGroup = _groupList.get(position);
+            if (contactGroup != null) {
+                holder.name = (TextView) convertView.findViewById(R.id.lv_topText);
+                holder.name.setText(contactGroup.name);
+            }
+            return convertView;
+        }
 
-		ContactGroup() {
-			Log.d(TAG, "A new ContactGroup()");
-		}
-	}
+        private Context mContext;
+    }
 
-	public class ContactGroupMember {
-		private static final String TAG = "ContactGroupMembersClass";
-		public String name;
-		public String mobilePhoneNumber;
-		public long id;
+    public class ViewHolder {
+        TextView name;
+    }
 
-		ContactGroupMember() {
-			Log.d(TAG, "A new ContactGroupMembersClass()");
-		}
-	}
+    public class ContactGroup {
+        private static final String TAG = "ContactGroupsClass";
+        public String name;
+        public long id;
+        public List<ContactGroupMember> memberIdList;
+
+        ContactGroup() {
+            Log.d(TAG, "A new ContactGroup()");
+        }
+    }
+
+    public class ContactGroupMember {
+        private static final String TAG = "ContactGroupMembersClass";
+        public String name;
+        public String mobilePhoneNumber;
+        public long id;
+
+        ContactGroupMember() {
+            Log.d(TAG, "A new ContactGroupMembersClass()");
+        }
+    }
 }
